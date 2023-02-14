d2e691886d983eabf017009feefdc21e50c92ada
Orel Adivi <orel.adivi@cs.technion.ac.il>
2023-02-14 18:15:49 +0200

    remove files

--- Firmaware/burn_micropython.bat
+++
@@ -1,8 +0,0 @@
-::
-:: %1 - connection to connect
-:: %2 - bin to burn
-::
-"C:\\Users\\Orel\\AppData\\Local\\Arduino15\\packages\\esp32\\tools\\esptool_py\\4.2.1/esptool.exe" --port %1 erase_flash
-"C:\\Users\\Orel\\AppData\\Local\\Arduino15\\packages\\esp32\\tools\\esptool_py\\4.2.1/esptool.exe" --port %1 write_flash 0x1000 %2
-echo.
-echo --- MicroPython BURNING WAS COMPLETED! ---
